bf07138bdf5c0048fe65d27ec45d4369cdb6b364
alanpereyra57 <alan@pereyra.online>
2020-03-08 04:40:25 -0300

    fix tutor ip style

--- templates/login.php
+++ templates/login.php
@@ -21,7 +21,7 @@ get_header();
 <?php do_action('tutor/template/login/before/wrap'); ?>
     <div <?php tutor_post_class('tutor-page-wrap'); ?>>
 
-        <div class="tutor-template-segment tutor-login-wrap">
+        <div class="bg-transparent-personalized tutor-template-segment tutor-login-wrap">
             <div class="tutor-login-title">
                 <h4><?php _e('Inicie sesión para ver esta sección', 'tutor'); ?></h4>
             </div>
